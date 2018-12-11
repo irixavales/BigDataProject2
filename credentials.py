9103a9d39645657d7e4938dbe036b04eb8979b09
irixa <irixa.vales@upr.edu>
2018-12-11 00:01:47 -0400

    first commit

---
+++ credentials.py
@@ -0,0 +1,6 @@
+credentials = {
+    'consumer_key': 'pyh86GmkzYN7fi2v44dwXBKP9',  # API key
+    'consumer_secret': 'jJZVr2WpIj0jhvCDC0VDR9A0ZHB47yVbbwATwYv5JmAKhsaORb',  # API secret key
+    'token': '1674358236-wsyPRtL3TwZnOVUS431uI7rYQ0YnU1FBLXGVJXl',  # Access token
+    'token_secret': 'J0zULXOsKmim1uafx0ELwWZkB9vtZTSp3wyM1HqzMkQWZ'  # Access token secret
+}
